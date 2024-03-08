0fbeed914a08bb049592dd4d166e0b3152d1f7ad
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-03-08 23:53:31 +0530

    Create check-if-frequency-can-be-equalin python-by-geeksfgeeks.py

---
+++ Day1/check-if-frequency-can-be-equalin python-by-geeksfgeeks.py
@@ -0,0 +1,33 @@
+# problem Statement: check frequency can be equal 
+class Solution:
+    
+    def isSame(self, freq, size):
+        c = 0
+        i = 0
+        for i in range(size):
+            if freq[i] > 0:
+                c = freq[i]
+                break
+        
+        for j in range(i + 1, size):
+            if freq[j] > 0 and freq[j] != c:
+                return False
+        
+        return True
+    
+    def sameFreq(self, s):
+        freq = [0] * 26
+        for c in s:
+            freq[ord(c) - ord('a')] += 1
+        
+        if self.isSame(freq, 26):
+            return True
+        
+        for i in range(26):
+            if freq[i] > 0:
+                freq[i] -= 1
+                if self.isSame(freq, 26):
+                    return True
+                freq[i] += 1
+        
+        return False
