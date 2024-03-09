f6e8745cb93695e15d57c32c825eae26adc90285
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-03-09 14:00:06 +0530

    Create minimum_common_value.py

---
+++ Day 2/minimum_common_value.py
@@ -0,0 +1,14 @@
+# problem Statement: minimum common value from given two list
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        
+        if not nums1 or not nums2:
+            return -1
+
+        common = nums1.intersection(nums2)
+        if not common:
+            return -1
+        else:
+            return min(common)
