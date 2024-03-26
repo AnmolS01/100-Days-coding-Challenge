128b0ceb8cff2534ca34b8426e4a847a4dde9f43
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-03-26 20:34:44 +0530

    Create First_missing_positive_by_LeetCode.py

---
+++ Day 19/First_missing_positive_by_LeetCode.py
@@ -0,0 +1,27 @@
+# Ptoblem Statement: 41. First Missing Positive
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 1
+
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] <= 0:
+                nums[i] = n + 1
+
+        for i in range(n):
+            num = abs(nums[i])
+            if num >=1 and num <= n:
+                nums[num - 1] = -abs(nums[num - 1])
+
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+
+        return n + 1
