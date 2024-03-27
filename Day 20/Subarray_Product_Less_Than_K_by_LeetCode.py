3ecf3bf8414d271975054b1f107f0e578f47215d
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-03-27 19:46:31 +0530

    Create Subarray_Product_Less_Than_K_by_LeetCode.py

---
+++ Day 20/Subarray_Product_Less_Than_K_by_LeetCode.py
@@ -0,0 +1,40 @@
+# 713. Subarray Product Less Than K
+# Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+# Example 1:
+
+# Input: nums = [10,5,2,6], k = 100
+# Output: 8
+# Explanation: The 8 subarrays that have product less than 100 are:
+# [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+# Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+# Example 2:
+
+# Input: nums = [1,2,3], k = 0
+# Output: 0
+#_________________________________________________________________________________CODE IS HERE__________________________________________________________________________________________
+
+class Solution(object):
+    def numSubarrayProductLessThanK(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if k <= 1:
+            return 0
+        
+        count = 0
+        n = len(nums)
+        start = 0
+        end = 0
+        product = 1
+
+        while end < n:
+            product = product * nums[end]
+            while product >= k:
+                product = product // nums[start]
+                start += 1
+            count += (end-start)+1
+            end += 1
+        return count
