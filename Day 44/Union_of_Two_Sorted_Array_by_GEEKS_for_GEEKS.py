bac2a33cd17ae521207e260e9ca306168c9f2ad2
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-04-20 21:32:00 +0530

    Create Union_of_Two_Sorted_Array_by_GEEKS_for_GEEKS.py

---
+++ Day 44/Union_of_Two_Sorted_Array_by_GEEKS_for_GEEKS.py
@@ -0,0 +1,17 @@
+class Solution:
+    
+    #Function to return a list containing the union of the two arrays.
+    def findUnion(self,arr1,arr2,n,m):
+        '''
+        :param a: given sorted array a
+        :param n: size of sorted array a
+        :param b: given sorted array b
+        :param m: size of sorted array b
+        :return:  The union of both arrays as a list
+        '''
+        # code here 
+        hashmap = set(arr1)
+        for val in arr2:
+            if val not in hashmap:
+                hashmap.add(val)
+        return sorted(hashmap)
