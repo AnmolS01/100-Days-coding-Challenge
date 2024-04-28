e0313b3ad9c602b7fac2d06919b65b6b8216e876
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-04-28 21:44:49 +0530

    Create Sums_of_Distances_in_Tree_by_LeetCode.py

---
+++ Day 52/Sums_of_Distances_in_Tree_by_LeetCode.py
@@ -0,0 +1,29 @@
+# Problem Statement : 834. Sum of Distances in Tree
+
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        count = [1] * n
+        ans = [0] * n
+        
+        def dfs1(node, parent):
+            for child in graph[node]:
+                if child != parent:
+                    dfs1(child, node)
+                    count[node] += count[child]
+                    ans[node] += ans[child] + count[child]
+        
+        def dfs2(node, parent):
+            for child in graph[node]:
+                if child != parent:
+                    ans[child] = ans[node] - count[child] + (n - count[child])
+                    dfs2(child, node)
+        
+        dfs1(0, -1)
+        dfs2(0, -1)
+        
+        return ans
