b5a9c5489dcd5f84dc81bc4f13096478c5ea5907
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-05-09 20:27:43 +0530

    Create Divisior_Game_by_GEEKS_for_GEEKS.py

---
+++ Day 63/Divisior_Game_by_GEEKS_for_GEEKS.py
@@ -0,0 +1,7 @@
+# Approach : if number is even, Alice have the chance to choose and Alice won.
+class Solution {
+    public static boolean divisorGame(int n) {
+        // code here
+        return n % 2 == 0;
+    }
+}
