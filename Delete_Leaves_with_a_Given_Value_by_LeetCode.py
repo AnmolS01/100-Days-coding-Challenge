218ce356e0e3c06e11d513956cf447613a22a5c7
AnmolS01 <153697581+AnmolS01@users.noreply.github.com>
2024-05-17 22:46:28 +0530

    Create Delete_Leaves_with_a_Given_Value_by_LeetCode.py

---
+++ Delete_Leaves_with_a_Given_Value_by_LeetCode.py
@@ -0,0 +1,14 @@
+# 1325. Delete Leaves With a Given Value
+
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        def dfs(root):
+            if not root:
+                return
+            root.left = dfs(root.left)
+            root.right = dfs(root.right)
+
+            if not root.left and not root.right and root.val == target:
+                return None
+            return root
+        return dfs(root)
